6a569e1e4c565d3888b0a68ade59b2865127bda7
Z-kamili <zakariakamili97@gmail.com>
2022-11-25 17:17:56 +0000

    add readme

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const EventEmitter = require('events');
+const http = require('http');
+const server = http.createServer((req,res) => {
+   if(req.urm === '/') {
+    res.write('HELLO WORLD');
+    res.end();
+   }
 
+    if(req.url === '/api/courses') {
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+
+});
+server.listen(3000);
+console.log('Listening on port 3000...');
 const Logger = require('./logger');
 const logger = new Logger();
-
 //Register a listener
 
 logger.on('messageLogged',(arg) => {
