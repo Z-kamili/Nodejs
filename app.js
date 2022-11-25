7c17d1da8de32289ab99ce89938f3ffe9c3b32ff
Z-kamili <zakariakamili97@gmail.com>
2022-11-25 16:05:15 +0000

    fix some problem

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
-
 // Register a listener
 emitter.on('messageLogged',(arg) => { // e, eventArg
     console.log('Listener called',arg);
 });
 
-//Raise an event
-emitter.emit('messageLogged',{id:1,url:'http://'});
+const log = require('./logger');
+
+log('message');
+
+
+//Raise: logging (data: message)
+
 
 
 
--- logger.js
+++ logger.js
@@ -1,12 +1,13 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
+
 var url = 'http://mylogger.io/log';
 
 function log(message) {
     // Send an HTTP request
     console.log(message);
     //Raise an event
-    emitter.emit('messageLogged', { id:1,url:'http://' });
+    emitter.emit('messageLogged',{id:1,url:'http://'});
 }
 
 module.exports = log;
